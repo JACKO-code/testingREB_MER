14a1bd734b3b8a149086ed0c66fcb171222dc987
Jannis Scholz <jannis.scholz@mail.schwarz>
2020-05-11 17:20:12 +0200

    file1

---
+++ file.ts
@@ -0,0 +1,37 @@
+class Familie{
+  constructor() {
+
+    let myString: string;
+    let blub: string = "unused";
+    
+    let yourWish: any = 'my name is jackson';
+    const mylength:any = myString.length;
+    let dadsf: any[]
+    let nsa: Array<string>;
+    let isGod: boolean;
+      let familie = [{ name: "jannis", age: 27, type: "male" }, {}, {}];
+        
+      
+    dadsf  = [
+        "dasds", "dasfasd",3,2,,23,4,34,2,42,,234,23,4,234,23,423
+    ];
+
+
+    let world: object;
+    
+  }
+
+  public(): string {
+
+    let c:any;
+
+    return "para" + myNumber ;//why not this.myNumber???? posibel;
+  }
+
+  private getAge(): boolean {
+    return true;
+  }
+
+
+}
+}
